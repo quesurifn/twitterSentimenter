dd502c9cd57c4e00f3b6f0f6538712f1aa4221a8
Kyle <kyle.c.r.fahey@gmail.com>
2017-09-27 10:42:58 -0500

    fix token

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var language = require('@google-cloud/language')({
   keyFilename: __dirname +'/Sentiment Analysis-1308e95a72be.json'
 });
 var analyzeIt = require("../analyzeIt.js");
-
+require('dotenv').config()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,10 +15,10 @@ router.get('/', function(req, res, next) {
 
 router.post("/postUsername", function(req, res){
   var client = new Twitter({
-    consumer_key: 'ExpY4m9IT20O75q0Dbib2NP92',
-    consumer_secret: 'BieIYVbvDKc0OrDCn5pNLuHD8HE3CctSonrFzkbnzYkflfeqJj',
-    access_token_key: '4207282659-kArtWlcBQ8ZNwC3R0d2Mr3YaWoE9tXVjVcupOWU',
-    access_token_secret: 'q4emfrOd8AXwWi2lYteadaRIhh8L01GIh6uJ2Fj0GHnoa'
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token,
+    access_token_secret: process.env.access_secret
   });// client
   client.get('statuses/user_timeline', {screen_name: req.body.userName, count:20}, function(error, tweets, response) {
     if (!error) {
@@ -48,6 +48,7 @@ router.post("/postUsername", function(req, res){
           res.send(result);//send to angular
         });//detect sentiment
       } else {
+        console.log(error)
       res.send("Invalid Username");
     }
   });//client get statuses
